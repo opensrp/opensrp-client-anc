5ad62a7e810e8f7564f0caf7fdc721c10fe61b47
Benjamin MWalimu <dubdabasoduba@gmail.com>
2018-08-17 11:15:55 +0300

    Remove unused import

--- opensrp-anc/src/test/java/org/smartregister/anc/presenter/RegisterFragmentPresenterTest.java
+++ opensrp-anc/src/test/java/org/smartregister/anc/presenter/RegisterFragmentPresenterTest.java
@@ -1,7 +1,6 @@
 package org.smartregister.anc.presenter;
 
 import org.json.JSONArray;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
