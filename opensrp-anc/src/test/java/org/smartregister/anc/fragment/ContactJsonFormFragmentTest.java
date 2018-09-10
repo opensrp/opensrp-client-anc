c8cb4b5d3e8c4ebb1bd93bc7de913084b6ac1e65
Martin Ndegwa <ndegwamartin@gmail.com>
2018-09-10 17:51:02 +0300

    Fix failing test

--- opensrp-anc/src/test/java/org/smartregister/anc/fragment/ContactJsonFormFragmentTest.java
+++ opensrp-anc/src/test/java/org/smartregister/anc/fragment/ContactJsonFormFragmentTest.java
@@ -1,14 +1,21 @@
 package org.smartregister.anc.fragment;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
 import com.vijay.jsonwizard.constants.JsonFormConstants;
 import com.vijay.jsonwizard.mvp.MvpBasePresenter;
 import com.vijay.jsonwizard.mvp.ViewState;
 import com.vijay.jsonwizard.presenters.JsonFormFragmentPresenter;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +23,7 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.smartregister.anc.R;
 import org.smartregister.anc.activity.BaseUnitTest;
 import org.smartregister.anc.util.DBConstants;
 import org.smartregister.anc.viewstate.ContactJsonFormFragmentViewState;
@@ -41,6 +49,21 @@ public class ContactJsonFormFragmentTest extends BaseUnitTest {
     @Mock
     private View view;
 
+    @Mock
+    private LinearLayout linearLayout;
+
+    @Mock
+    private ScrollView scrollView;
+
+    @Mock
+    private Button button;
+
+    @Mock
+    private ImageView imageView;
+
+    @Mock
+    private ActionBar actionBar;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -55,10 +78,21 @@ public class ContactJsonFormFragmentTest extends BaseUnitTest {
         formFragment.onOptionsItemSelected(menuItem);
 
         ViewGroup nullViewGroup = null;
-        Mockito.doReturn(view).when(layoutInflater).inflate(ArgumentMatchers.eq(com.vijay.jsonwizard.R.layout.native_form_fragment_json_wizard), ArgumentMatchers.eq(nullViewGroup));
-        formFragment.onCreateView(layoutInflater, viewGroup, bundle);
+        Mockito.doReturn(view).when(layoutInflater).inflate(ArgumentMatchers.eq(R.layout.contact_form_fragment_json_wizard), ArgumentMatchers.eq(nullViewGroup));
+        Mockito.doReturn(scrollView).when(view).findViewById(R.id.scroll_view);
+        Mockito.doReturn(linearLayout).when(view).findViewById(R.id.main_layout);
+        Mockito.doReturn(button).when(view).findViewById(R.id.previous);
+        Mockito.doReturn(button).when(view).findViewById(R.id.next);
+        Mockito.doReturn(imageView).when(view).findViewById(R.id.previous_icon);
+        Mockito.doReturn(imageView).when(view).findViewById(R.id.next_icon);
 
-        Bundle fragmentArgs = formFragment.getArguments();
+        ContactJsonFormFragment formFragmentSpy = Mockito.spy(formFragment);
+
+        Mockito.doReturn(actionBar).when(formFragmentSpy).getSupportActionBar();
+
+        formFragmentSpy.onCreateView(layoutInflater, viewGroup, bundle);
+
+        Bundle fragmentArgs = formFragmentSpy.getArguments();
         Assert.assertEquals(JsonFormConstants.FIRST_STEP_NAME, fragmentArgs.get(DBConstants.KEY.STEPNAME));
     }
 
