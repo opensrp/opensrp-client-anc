3d2bd0d3a7e4362aba16c7cffc55515606ce0e15
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2018-10-18 20:22:27 +0300

    Add mefragment tests
    Signed-off-by: Benjamin Mwalimu <dubdabasoduba@gmail.com>

--- opensrp-anc/src/test/java/org/smartregister/anc/fragment/NoMatchDialogFragmentTest.java
+++ opensrp-anc/src/test/java/org/smartregister/anc/fragment/NoMatchDialogFragmentTest.java
@@ -1,22 +1,43 @@
 package org.smartregister.anc.fragment;
 
 import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
+import org.robolectric.android.controller.ActivityController;
 import org.smartregister.anc.activity.BaseUnitTest;
+import org.smartregister.anc.activity.HomeRegisterActivity;
 
 public class NoMatchDialogFragmentTest extends BaseUnitTest {
-	
-	@Before
-	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-	}
-	
-	@Test
-	public void testLaunchDialogShouldReturnNullIfActivityIsNotSet() {
-		NoMatchDialogFragment noMatchDialogFragment = NoMatchDialogFragment.launchDialog(null,null,null);
-		Assert.assertNull(noMatchDialogFragment);
-		
-	}
+    private HomeRegisterActivity activity;
+    private ActivityController<HomeRegisterActivity> controller;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = Robolectric.buildActivity(HomeRegisterActivity.class).create().start();
+        activity = controller.get();
+    }
+
+    @Test
+    public void testLaunchDialogShouldReturnNullIfActivityIsNotSet() {
+        NoMatchDialogFragment noMatchDialogFragment = NoMatchDialogFragment.launchDialog(null, null, null);
+        Assert.assertNull(noMatchDialogFragment);
+
+    }
+
+    @Test
+    public void testActivityIsInstantiatedCorrectly() {
+        Assert.assertNotNull(activity);
+        Assert.assertTrue(activity.isFinishing());
+    }
+
+    @Test
+    public void testLaunchDialogShouldReturnNoMatchDialogFragmentIfActivityIsNotNull() {
+        NoMatchDialogFragment noMatchDialogFragment = NoMatchDialogFragment.launchDialog(activity, "", "11234");
+        Assert.assertNotNull(noMatchDialogFragment);
+
+    }
 }
