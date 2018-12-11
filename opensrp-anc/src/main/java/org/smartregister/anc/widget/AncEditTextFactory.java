92400f9b359c8ea2c9c82932dd49f235e006c07f
Elly Kitoto <ekitoto@ona.io>
2018-12-11 15:22:30 +0300

    Added more relevance on Profile contact form on section 3

--- opensrp-anc/src/main/java/org/smartregister/anc/widget/AncEditTextFactory.java
+++ opensrp-anc/src/main/java/org/smartregister/anc/widget/AncEditTextFactory.java
@@ -5,6 +5,7 @@ import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
@@ -27,11 +28,14 @@ import java.util.List;
  * Created by ndegwamartin on 30/06/2018.
  */
 public class AncEditTextFactory extends EditTextFactory {
+    private ImageView imageView;
 
     @Override
-    public void attachJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, MaterialEditText editText) throws Exception {
-        super.attachJson(stepName, context, formFragment, jsonObject, editText);
+    public void attachLayout(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, MaterialEditText editText, ImageView imageView) throws Exception {
+        super.attachLayout(stepName, context, formFragment, jsonObject, editText, imageView);
+        this.imageView = imageView;
     }
+
 
     @Override
     public List<View> getViewsFromJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, CommonListener listener) throws Exception {
@@ -41,7 +45,7 @@ public class AncEditTextFactory extends EditTextFactory {
             RelativeLayout rootLayout = getRootLayout(context);
             final MaterialEditText editText = rootLayout.findViewById(R.id.edit_text);
 
-            attachJson(stepName, context, formFragment, jsonObject, editText);
+            attachLayout(stepName, context, formFragment, jsonObject, editText,this.imageView);
 
             JSONArray canvasIds = new JSONArray();
             rootLayout.setId(ViewUtil.generateViewId());
