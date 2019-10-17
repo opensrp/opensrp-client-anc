510e2f2c7a57a8775207da2baea6c81652ac093c
bennsimon <botieno@ona.io>
2019-10-17 13:01:21 +0300

    added template utils test and fixed some test

--- opensrp-anc/src/main/java/org/smartregister/anc/library/interactor/CharacteristicsInteractor.java
+++ opensrp-anc/src/main/java/org/smartregister/anc/library/interactor/CharacteristicsInteractor.java
@@ -1,5 +1,7 @@
 package org.smartregister.anc.library.interactor;
 
+import android.content.Context;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -7,11 +9,15 @@ import org.smartregister.AllConstants;
 import org.smartregister.anc.library.AncLibrary;
 import org.smartregister.anc.library.contract.SiteCharacteristicsContract;
 import org.smartregister.anc.library.util.ConstantsUtils;
+import org.smartregister.anc.library.util.SiteCharacteristicsFormUtils;
 import org.smartregister.domain.Setting;
 import org.smartregister.domain.SyncStatus;
 import org.smartregister.repository.AllSettings;
+import org.smartregister.util.Utils;
 
 import java.util.Map;
+
+import timber.log.Timber;
 
 /**
  * Created by ndegwamartin on 13/07/2018.
@@ -22,40 +28,49 @@ public class CharacteristicsInteractor implements SiteCharacteristicsContract.In
     public void saveSiteCharacteristics(Map<String, String> siteCharacteristicsSettingsMap) throws JSONException {
 
         JSONArray localSettings;
+        JSONObject settingObject;
+        Context context = AncLibrary.getInstance().getApplicationContext();
         Setting characteristic = getAllSettingsRepo().getSetting(ConstantsUtils.PrefKeyUtils.SITE_CHARACTERISTICS);
+        boolean canSaveInitialSetting = getPropertyForInitialSaveAction(context);
+        if (characteristic == null) {
 
-
-        JSONObject settingObject = characteristic != null ? new JSONObject(characteristic.getValue()) : null;
-        if (settingObject != null) {
-            localSettings = settingObject.getJSONArray(AllConstants.SETTINGS);
-
-            if (localSettings != null) {
-
-                for (int i = 0; i < localSettings.length(); i++) {
-                    JSONObject localSetting = localSettings.getJSONObject(i);
-
-                    //updating by java object reference
-                    localSetting.put(ConstantsUtils.KeyUtils.VALUE,
-                            "1".equals(siteCharacteristicsSettingsMap.get(localSetting.getString(ConstantsUtils.KeyUtils.KEY))));
-
+            if (canSaveInitialSetting) {
+                try {
+                    settingObject = SiteCharacteristicsFormUtils.structureFormForRequest(context);
+                    characteristic = new Setting();
+                } catch (Exception e) {
+                    Timber.e(e);
+                    return;
                 }
-
+            } else {
+                return;
             }
-
-            settingObject.put(AllConstants.SETTINGS, localSettings);
-            characteristic.setValue(settingObject.toString());
-            characteristic
-                    .setKey(ConstantsUtils.PrefKeyUtils.SITE_CHARACTERISTICS); //We know only site characteristics are being saved
-            // at this time
-            characteristic.setSyncStatus(SyncStatus.PENDING.name());
-
-            getAllSettingsRepo().putSetting(characteristic);
-
-            AncLibrary.getInstance().populateGlobalSettings();//Refresh global settings
+        } else {
+            settingObject = new JSONObject(characteristic.getValue());
         }
+        localSettings = settingObject.has(AllConstants.SETTINGS) ? settingObject.getJSONArray(AllConstants.SETTINGS) : null;
+        if (localSettings != null) {
+            for (int i = 0; i < localSettings.length(); i++) {
+                JSONObject localSetting = localSettings.getJSONObject(i);
+                localSetting.put(ConstantsUtils.KeyUtils.VALUE,
+                        "1".equals(siteCharacteristicsSettingsMap.get(localSetting.getString(ConstantsUtils.KeyUtils.KEY))));
+            }
+        }
+
+        settingObject.put(AllConstants.SETTINGS, localSettings);
+        characteristic.setValue(settingObject.toString());
+        characteristic.setKey(ConstantsUtils.PrefKeyUtils.SITE_CHARACTERISTICS);
+        characteristic.setSyncStatus(SyncStatus.PENDING.name());
+        getAllSettingsRepo().putSetting(characteristic);
+        AncLibrary.getInstance().populateGlobalSettings();
     }
 
     protected AllSettings getAllSettingsRepo() {
         return AncLibrary.getInstance().getContext().allSettings();
+    }
+
+    private Boolean getPropertyForInitialSaveAction(Context context) {
+        String value = Utils.getProperties(context).getProperty(ConstantsUtils.Properties.CAN_SAVE_INITIAL_SETTING, "false");
+        return Boolean.valueOf(value);
     }
 }
---
+++ opensrp-anc/src/test/java/org/smartregister/anc/library/util/TemplateUtilsTest.java
@@ -0,0 +1,39 @@
+package org.smartregister.anc.library.util;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+
+@RunWith(PowerMockRunner.class)
+public class TemplateUtilsTest {
+
+    @Test
+    public void testGetTemplateAsJsonShouldReturnEmptyJson() throws IOException {
+        Context context = PowerMockito.mock(Context.class);
+        Resources resources = PowerMockito.mock(Resources.class);
+        Configuration configuration = PowerMockito.mock(Configuration.class);
+        AssetManager assetManager = Mockito.spy(AssetManager.class);
+        configuration.locale = Locale.getDefault();
+        PowerMockito.when(resources.getConfiguration()).thenReturn(configuration);
+        InputStream inputStream = new ByteArrayInputStream("{}".getBytes());
+
+        PowerMockito.when(assetManager.open("template" + "/" + "templateName" + ".json")).thenReturn(inputStream);
+        PowerMockito.when(context.getResources()).thenReturn(resources);
+        PowerMockito.when(context.getAssets()).thenReturn(assetManager);
+
+        Assert.assertEquals(0, TemplateUtils.getTemplateAsJson(context, "templateName").length());
+    }
+}
