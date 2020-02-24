146208adcfdb40902d4b670479414843fbc035ff
Ephraim Kigamba <ekigamba@ona.io>
2020-02-24 15:46:47 +0300

    Fix codacy issue

--- opensrp-anc/src/test/java/org/smartregister/anc/library/sync/BaseAncClientProcessorForJavaTest.java
+++ opensrp-anc/src/test/java/org/smartregister/anc/library/sync/BaseAncClientProcessorForJavaTest.java
@@ -1,12 +1,12 @@
 package org.smartregister.anc.library.sync;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.robolectric.RuntimeEnvironment;
 import org.smartregister.anc.library.activity.BaseUnitTest;
 import org.smartregister.anc.library.util.ConstantsUtils;
 
-import static org.junit.Assert.*;
 
 public class BaseAncClientProcessorForJavaTest extends BaseUnitTest {
 
@@ -19,16 +19,16 @@ public class BaseAncClientProcessorForJavaTest extends BaseUnitTest {
 
     @Test
     public void canProcessShouldReturnTrueWhenProvidedAncEventTypes() {
-        assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.REGISTRATION));
-        assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.UPDATE_REGISTRATION));
-        assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.QUICK_CHECK));
-        assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.CONTACT_VISIT));
-        assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.CLOSE));
-        assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.SITE_CHARACTERISTICS));
+        Assert.assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.REGISTRATION));
+        Assert.assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.UPDATE_REGISTRATION));
+        Assert.assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.QUICK_CHECK));
+        Assert.assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.CONTACT_VISIT));
+        Assert.assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.CLOSE));
+        Assert.assertTrue(baseAncClientProcessorForJava.canProcess(ConstantsUtils.EventTypeUtils.SITE_CHARACTERISTICS));
     }
 
     @Test
     public void getEventTypesShouldReturn6EvenTypes() {
-        assertEquals(6, baseAncClientProcessorForJava.getEventTypes().size());
+        Assert.assertEquals(6, baseAncClientProcessorForJava.getEventTypes().size());
     }
 }
