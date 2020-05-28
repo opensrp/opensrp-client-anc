d8ea19d410b5bd5b5225202b23dd5f3d2dd6c3bd
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-05-28 14:02:40 +0300

    :zap: added the setitngs API new endpoints

--- reference-app/src/main/java/org/smartregister/anc/application/AncSyncConfiguration.java
+++ reference-app/src/main/java/org/smartregister/anc/application/AncSyncConfiguration.java
@@ -4,8 +4,10 @@ import org.smartregister.SyncConfiguration;
 import org.smartregister.SyncFilter;
 import org.smartregister.anc.BuildConfig;
 import org.smartregister.anc.library.AncLibrary;
+import org.smartregister.anc.library.util.ConstantsUtils;
 import org.smartregister.repository.AllSharedPreferences;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,6 +32,26 @@ public class AncSyncConfiguration extends SyncConfiguration {
     @Override
     public boolean resolveSettings() {
         return BuildConfig.RESOLVE_SETTINGS;
+    }
+
+    @Override
+    public boolean hasGlobalSettings() {
+        return BuildConfig.HAS_GLOBAL_SETTINGS;
+    }
+
+    @Override
+    public boolean hasExtraSettingsSync() {
+        return BuildConfig.HAS_SETTINGS_SYNC;
+    }
+
+    @Override
+    public List<String> getExtraSettingsParameters() {
+        AllSharedPreferences sharedPreferences = AncLibrary.getInstance().getContext().userService().getAllSharedPreferences();
+        String providerId = sharedPreferences.fetchRegisteredANM();
+
+        List<String> params = new ArrayList<>();
+        params.add(ConstantsUtils.SettingsSyncParams.LOCATION_ID + "=" + sharedPreferences.fetchDefaultLocalityId(providerId));
+        return params;
     }
 
     @Override
