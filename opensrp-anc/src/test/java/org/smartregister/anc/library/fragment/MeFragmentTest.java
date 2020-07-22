d2cc23602399b82f2f82c5ff7839fbcde06be62e
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-07-22 16:07:15 +0300

    :construction: update the repositories

--- opensrp-anc/src/test/java/org/smartregister/anc/library/fragment/MeFragmentTest.java
+++ opensrp-anc/src/test/java/org/smartregister/anc/library/fragment/MeFragmentTest.java
@@ -1,16 +1,22 @@
 package org.smartregister.anc.library.fragment;
 
+import android.app.Activity;
+
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.android.controller.ActivityController;
 import org.smartregister.anc.library.AncLibrary;
-import org.smartregister.anc.library.activity.BaseUnitTest;
+import org.smartregister.anc.library.activity.BaseActivityUnitTest;
+import org.smartregister.anc.library.activity.BaseHomeRegisterActivity;
 
-public class MeFragmentTest extends BaseUnitTest {
+public class MeFragmentTest extends BaseActivityUnitTest {
     @Mock
     private AncLibrary ancLibrary;
-
+    private BaseHomeRegisterActivity activity;
     private MeFragment meFragment;
 
     @Before
@@ -18,4 +24,20 @@ public class MeFragmentTest extends BaseUnitTest {
         MockitoAnnotations.initMocks(this);
         meFragment = Mockito.spy(MeFragment.class);
     }
+
+    @Override
+    protected Activity getActivity() {
+        return null;
+    }
+
+    @Override
+    protected ActivityController getActivityController() {
+        return null;
+    }
+
+    @Test
+    public void testActivityIsInstantiatedCorrectly() {
+        Assert.assertNotNull(activity);
+    }
+
 }
