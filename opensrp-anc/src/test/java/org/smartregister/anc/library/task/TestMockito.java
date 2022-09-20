9c8560f1e26a69f80366e3680fefbbcb2cb5de78
SebaMutuku <36365043+SebaMutuku@users.noreply.github.com>
2022-09-20 16:18:33 +0300

    Jacoco compatibility issues

---
+++ opensrp-anc/src/test/java/org/smartregister/anc/library/task/TestMockito.java
@@ -0,0 +1,16 @@
+package org.smartregister.anc.library.task;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.smartregister.anc.library.activity.BaseUnitTest;
+
+public class TestMockito extends BaseUnitTest {
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testMockito() {
+
+    }
+}
