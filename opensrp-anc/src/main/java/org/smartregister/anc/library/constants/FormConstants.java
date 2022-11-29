a74607bc93612c756bc95f192a9f5fba72cf606e
Aria Dhanang <dhanang@bahtera.co>
2022-11-29 07:00:27 +0700

    Customize registration form fields

---
+++ opensrp-anc/src/main/java/org/smartregister/anc/library/constants/FormConstants.java
@@ -0,0 +1,19 @@
+package org.smartregister.anc.library.constants;
+
+public class FormConstants {
+
+    public final static class Client {
+
+        // Identifiers
+        public static final String IDENTIFIER_UID = "uid";
+        public static final String IDENTIFIER_SSN = "ssn";
+
+        // Attributes
+        public static final String ATTRIBUTE_UID_UNKNOWN = "uid_unknown";
+        public static final String ATTRIBUTE_UID_UNKNOWN_REASON = "uid_unknown_reason";
+        public static final String ATTRIBUTE_PHONE_UNKNOWN = "phone_number_unknown";
+        public static final String ATTRIBUTE_PHONE_UNKNOWN_REASON = "phone_number_unknown_reason";
+
+    }
+
+}
