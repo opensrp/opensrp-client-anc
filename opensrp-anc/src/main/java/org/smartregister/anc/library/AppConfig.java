76f0b275df9ef96613225c756ae03b7c245b94d6
Aria Dhanang <ariadhanang@gmail.com>
2023-01-26 22:34:53 +0700

    Update Site Characteristics form

--- opensrp-anc/src/main/java/org/smartregister/anc/library/AppConfig.java
+++ opensrp-anc/src/main/java/org/smartregister/anc/library/AppConfig.java
@@ -1,6 +1,8 @@
 package org.smartregister.anc.library;
 
 import org.smartregister.anc.library.constants.Constants;
+import org.smartregister.anc.library.util.Utils;
+import org.smartregister.util.LangUtils;
 
 import java.util.Locale;
 
@@ -9,5 +11,6 @@ public class AppConfig {
 
 	/** The default locale of the app for language translations & locale-specific resources. */
 	public static final Locale DefaultLocale = Constants.Locales.INDONESIAN_ID;
+
 
 }
