924943ccaeb4834783587c017c43f7a7794bbed9
Aria Dhanang <ariadhanang@gmail.com>
2023-01-27 13:02:50 +0700

    Fix contact required fields count bug

--- opensrp-anc/src/main/java/org/smartregister/anc/library/activity/MainContactActivity.java
+++ opensrp-anc/src/main/java/org/smartregister/anc/library/activity/MainContactActivity.java
@@ -3,6 +3,7 @@ package org.smartregister.anc.library.activity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -108,12 +109,8 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
             quickCheck.setBackground(R.drawable.quick_check_bg);
             quickCheck.setWizard(false);
             quickCheck.setHideSaveLabel(true);
-            if (requiredFieldsMap.containsKey(quickCheck.getName())) {
-                Integer quickCheckFields = requiredFieldsMap.get(quickCheck.getName());
-                quickCheck.setRequiredFields(quickCheckFields != null ? quickCheckFields : 0);
-            }
-
             quickCheck.setFormName(ConstantsUtils.JsonFormUtils.ANC_QUICK_CHECK);
+            quickCheck.setRequiredFields(0);
             contacts.add(quickCheck);
 
             Contact profile = new Contact();
@@ -122,8 +119,8 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
             profile.setBackground(R.drawable.profile_bg);
             profile.setActionBarBackground(R.color.contact_profile_actionbar);
             profile.setNavigationBackground(R.color.contact_profile_navigation);
-            setRequiredFields(profile);
             profile.setFormName(ConstantsUtils.JsonFormUtils.ANC_PROFILE);
+            setRequiredFields(profile);
             contacts.add(profile);
 
             Contact symptomsAndFollowUp = new Contact();
@@ -132,8 +129,8 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
             symptomsAndFollowUp.setBackground(R.drawable.symptoms_bg);
             symptomsAndFollowUp.setActionBarBackground(R.color.contact_symptoms_actionbar);
             symptomsAndFollowUp.setNavigationBackground(R.color.contact_symptoms_navigation);
-            setRequiredFields(symptomsAndFollowUp);
             symptomsAndFollowUp.setFormName(ConstantsUtils.JsonFormUtils.ANC_SYMPTOMS_FOLLOW_UP);
+            setRequiredFields(symptomsAndFollowUp);
             contacts.add(symptomsAndFollowUp);
 
             Contact physicalExam = new Contact();
@@ -142,8 +139,8 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
             physicalExam.setBackground(R.drawable.physical_exam_bg);
             physicalExam.setActionBarBackground(R.color.contact_exam_actionbar);
             physicalExam.setNavigationBackground(R.color.contact_exam_navigation);
-            setRequiredFields(physicalExam);
             physicalExam.setFormName(ConstantsUtils.JsonFormUtils.ANC_PHYSICAL_EXAM);
+            setRequiredFields(physicalExam);
             contacts.add(physicalExam);
 
             Contact tests = new Contact();
@@ -152,8 +149,8 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
             tests.setBackground(R.drawable.tests_bg);
             tests.setActionBarBackground(R.color.contact_tests_actionbar);
             tests.setNavigationBackground(R.color.contact_tests_navigation);
-            setRequiredFields(tests);
             tests.setFormName(ConstantsUtils.JsonFormUtils.ANC_TEST);
+            setRequiredFields(tests);
             contacts.add(tests);
 
             Contact counsellingAndTreatment = new Contact();
@@ -162,8 +159,8 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
             counsellingAndTreatment.setBackground(R.drawable.counselling_bg);
             counsellingAndTreatment.setActionBarBackground(R.color.contact_counselling_actionbar);
             counsellingAndTreatment.setNavigationBackground(R.color.contact_counselling_navigation);
-            setRequiredFields(counsellingAndTreatment);
             counsellingAndTreatment.setFormName(ConstantsUtils.JsonFormUtils.ANC_COUNSELLING_TREATMENT);
+            setRequiredFields(counsellingAndTreatment);
             contacts.add(counsellingAndTreatment);
 
             contactAdapter.setContacts(contacts);
@@ -201,6 +198,7 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
     private void process(String[] mainContactForms) {
         //Fetch and load previously saved values
         try {
+
             if (contactNo > 1 && !PatientRepository.isFirstVisit(baseEntityId)) {
                 for (String formEventType : new ArrayList<>(Arrays.asList(mainContactForms))) {
                     if (eventToFileMap.containsValue(formEventType)) {
@@ -243,8 +241,8 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
     }
 
     private void setRequiredFields(Contact contact) {
-        if (requiredFieldsMap != null && contact != null && requiredFieldsMap.containsKey(contact.getName())) {
-            contact.setRequiredFields(requiredFieldsMap.get(contact.getName()));
+        if (requiredFieldsMap != null && contact != null && requiredFieldsMap.containsKey(contact.getFormName())) {
+            contact.setRequiredFields(requiredFieldsMap.get(contact.getFormName()));
         }
     }
 
@@ -354,6 +352,7 @@ public class MainContactActivity extends BaseContactActivity implements ContactC
     }
 
     private void updateFieldRequiredCount(JSONObject object, JSONObject fieldObject, boolean isRequiredField) throws JSONException {
+
         if (isRequiredField && (!fieldObject.has(JsonFormConstants.VALUE) ||
                 TextUtils.isEmpty(fieldObject.getString(JsonFormConstants.VALUE)))) {
             Integer requiredFieldCount = requiredFieldsMap.get(object.getString(ConstantsUtils.JsonFormKeyUtils.ENCOUNTER_TYPE));
