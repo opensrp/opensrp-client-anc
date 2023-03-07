c605221df8093e01c13f6547e400d0e72d2eecc0
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2023-03-07 16:07:05 +0500

    Migrate services to workmanager

--- reference-app/src/main/java/org/smartregister/anc/interactor/LoginInteractor.java
+++ reference-app/src/main/java/org/smartregister/anc/interactor/LoginInteractor.java
@@ -1,16 +1,22 @@
 package org.smartregister.anc.interactor;
 
+import android.content.Context;
+
+import androidx.work.Data;
+
+import org.smartregister.CoreLibrary;
 import org.smartregister.anc.BuildConfig;
 import org.smartregister.anc.library.AncLibrary;
 import org.smartregister.domain.LoginResponse;
-import org.smartregister.job.DocumentConfigurationServiceJob;
-import org.smartregister.job.ImageUploadServiceJob;
-import org.smartregister.job.P2pServiceJob;
-import org.smartregister.job.PullUniqueIdsServiceJob;
-import org.smartregister.job.SyncAllLocationsServiceJob;
-import org.smartregister.job.SyncServiceJob;
-import org.smartregister.job.SyncSettingsServiceJob;
 import org.smartregister.login.interactor.BaseLoginInteractor;
+import org.smartregister.sync.wm.worker.DocumentConfigurationWorker;
+import org.smartregister.sync.wm.worker.ImageUploadWorkerWorker;
+import org.smartregister.sync.wm.worker.P2pProcessRecordsWorker;
+import org.smartregister.sync.wm.worker.PullUniqueIdsWorker;
+import org.smartregister.sync.wm.worker.SettingsSyncWorker;
+import org.smartregister.sync.wm.worker.SyncAllLocationsWorker;
+import org.smartregister.sync.wm.worker.SyncWorker;
+import org.smartregister.sync.wm.workerrequest.WorkRequest;
 import org.smartregister.view.contract.BaseLoginContract;
 
 import java.util.concurrent.TimeUnit;
@@ -25,29 +31,33 @@ public class LoginInteractor extends BaseLoginInteractor implements BaseLoginCon
 
     @Override
     protected void scheduleJobsPeriodically() {
-        SyncServiceJob.scheduleJob(SyncServiceJob.TAG, TimeUnit.MINUTES.toMillis(BuildConfig.DATA_SYNC_DURATION_MINUTES),
-                getFlexValue(BuildConfig.DATA_SYNC_DURATION_MINUTES));
-        PullUniqueIdsServiceJob
-                .scheduleJob(PullUniqueIdsServiceJob.TAG, TimeUnit.MINUTES.toMillis(BuildConfig.PULL_UNIQUE_IDS_MINUTES),
-                        getFlexValue(BuildConfig.PULL_UNIQUE_IDS_MINUTES));
-        ImageUploadServiceJob
-                .scheduleJob(ImageUploadServiceJob.TAG, TimeUnit.MINUTES.toMillis(BuildConfig.IMAGE_UPLOAD_MINUTES),
-                        getFlexValue(BuildConfig.IMAGE_UPLOAD_MINUTES));
-        SyncSettingsServiceJob
-                .scheduleJob(SyncSettingsServiceJob.TAG, TimeUnit.MINUTES.toMillis(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES),
-                        getFlexValue(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES));
-        DocumentConfigurationServiceJob
-                .scheduleJob(DocumentConfigurationServiceJob.TAG, TimeUnit.MINUTES.toMillis(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES),
-                        getFlexValue(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES));
-        P2pServiceJob.scheduleJob(P2pServiceJob.TAG, TimeUnit.MINUTES.toMillis(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES),
-                        getFlexValue(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES));
+        Context applicationContext = CoreLibrary.getInstance().context().applicationContext();
+        WorkRequest.runPeriodically(applicationContext, SyncWorker.class, SyncWorker.TAG,
+                BuildConfig.DATA_SYNC_DURATION_MINUTES, getFlexValue(BuildConfig.DATA_SYNC_DURATION_MINUTES),
+                TimeUnit.MINUTES, Data.EMPTY);
+        WorkRequest.runPeriodically(applicationContext, PullUniqueIdsWorker.class, PullUniqueIdsWorker.TAG,
+                BuildConfig.PULL_UNIQUE_IDS_MINUTES, getFlexValue(BuildConfig.PULL_UNIQUE_IDS_MINUTES),
+                TimeUnit.MINUTES, Data.EMPTY);
+        WorkRequest.runPeriodically(applicationContext, ImageUploadWorkerWorker.class, ImageUploadWorkerWorker.TAG,
+                BuildConfig.IMAGE_UPLOAD_MINUTES, getFlexValue(BuildConfig.IMAGE_UPLOAD_MINUTES),
+                TimeUnit.MINUTES, Data.EMPTY);
+        WorkRequest.runPeriodically(applicationContext, SettingsSyncWorker.class, SettingsSyncWorker.TAG,
+                BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES, getFlexValue(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES),
+                TimeUnit.MINUTES, Data.EMPTY);
+        WorkRequest.runPeriodically(applicationContext, DocumentConfigurationWorker.class, DocumentConfigurationWorker.TAG,
+                BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES, getFlexValue(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES),
+                TimeUnit.MINUTES, Data.EMPTY);
+        WorkRequest.runPeriodically(applicationContext, P2pProcessRecordsWorker.class, P2pProcessRecordsWorker.TAG,
+                BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES, getFlexValue(BuildConfig.CLIENT_SETTINGS_SYNC_MINUTES),
+                TimeUnit.MINUTES, Data.EMPTY);
     }
 
     @Override
     protected void scheduleJobsImmediately() {
         super.scheduleJobsImmediately();
-        DocumentConfigurationServiceJob.scheduleJobImmediately(DocumentConfigurationServiceJob.TAG);
-        SyncAllLocationsServiceJob.scheduleJobImmediately(SyncAllLocationsServiceJob.TAG);
+        Context applicationContext = CoreLibrary.getInstance().context().applicationContext();
+        WorkRequest.runImmediately(applicationContext, DocumentConfigurationWorker.class, DocumentConfigurationWorker.TAG, Data.EMPTY);
+        WorkRequest.runImmediately(applicationContext, SyncAllLocationsWorker.class, SyncAllLocationsWorker.TAG, Data.EMPTY);
     }
 
     @Override
