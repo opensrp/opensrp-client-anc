24f26a16d9dbba16aa1b4e88c2abc5f55e949604
Naima-Bashir <nbashir@ona.io>
2024-07-16 17:31:03 +0300

    Tests for Contact Visit 1

---
+++ reference-app/src/androidTest/java/org/smartregister/anc/activity/anc/ContactsActivityTest.java
@@ -0,0 +1,55 @@
+package org.smartregister.anc.activity.anc;
+
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.smartregister.anc.R;
+import org.smartregister.anc.activity.LoginActivity;
+import org.smartregister.anc.activity.utils.Configs;
+import org.smartregister.anc.activity.utils.Constants;
+import org.smartregister.anc.activity.utils.Utils;
+
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(AndroidJUnit4.class)
+public class ContactsActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> mActivityScenario = new ActivityScenarioRule<>(LoginActivity.class);
+
+    private Utils utils = new Utils();
+
+    @Test
+    public void A_setUp() throws InterruptedException {
+        utils.logIn(Constants.ancConstants.ancUsername, Constants.ancConstants.ancPassword);
+    }
+
+    @Test
+    public void B_StartContactVisit() throws InterruptedException {
+
+        Thread.sleep(2000);
+
+        onView(withId(R.id.edt_search)).perform(typeText(Configs.TestDataConfigs.clientName), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.patient_name)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.due_button_wrapper)).perform(click());
+        Thread.sleep(2000);
+
+    }
+}
--- reference-app/src/androidTest/java/org/smartregister/anc/activity/utils/Configs.java
+++ reference-app/src/androidTest/java/org/smartregister/anc/activity/utils/Configs.java
@@ -3,8 +3,8 @@ public class Configs {
 
 
  public static class TestDataConfigs{
-     public static final String clientName = "Lauren Hill";
-     public static final String clientID = "7138845";
+     public static final String clientName = "Beth Kasin";
+     public static final String clientID = "7187941";
 
  }
 }
