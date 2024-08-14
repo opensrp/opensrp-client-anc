69c3d07f856abefe2882f15e7472e8bf69838a47
Karina Brian <bkarinamuraya@gmail.com>
2024-08-14 11:28:42 +0300

    Updated constants

--- reference-app/src/androidTest/java/org/smartregister/anc/activity/utils/Constants.java
+++ reference-app/src/androidTest/java/org/smartregister/anc/activity/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     //values to be input each time a test is run
 
-    public static final String ancUsername = "demo";
-    public static final String ancPassword = "Amani123";
+    public static final String ancUsername = "";
+    public static final String ancPassword = "";
     }
 }
